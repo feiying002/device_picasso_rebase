730b59a407b6bf24ce17170a043afa5f5165cde6
hiper25 <2243168976@qq.com>
2021-05-20 17:11:37 +0000

    picasso: Include all build hacks
    * BUILD_BROKEN_DUP_RULES := true
    * BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
    * BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,11 @@ TARGET_OTA_ASSERT_DEVICE := picasso
 TARGET_BOOTLOADER_BOARD_NAME := lito
 TARGET_NO_BOOTLOADER := true
 
+# Broken Rules
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+BUILD_BROKEN_PREBUILT_ELF_FILES := true
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
